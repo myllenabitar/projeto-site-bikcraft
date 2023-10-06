a7dc96f51ccd91e5c2871fcf9d2a91615385e162
myllenabitar <myllena.odo@gmail.com>
2023-10-06 15:20:55 -0400

    js na parte de imagens de bicicletas

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 // Ativar links do menu
-
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link){
@@ -13,7 +12,6 @@ function ativarLink(link){
 links.forEach(ativarLink);
 
 // Ativar itens do orçamento
-
 const parametros = new URLSearchParams(location.search);
 
 function ativarProduto(parametro) {
@@ -22,9 +20,28 @@ function ativarProduto(parametro) {
         elemento.checked = true; 
     }  
 }
-
 parametros.forEach(ativarProduto);
 
-
 // Perguntas frequentes
+const perguntas = document.querySelectorAll('.perguntas button');
 
+function ativarPergunta(event){
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle("ativa");
+    const ativa = resposta.classList.contains("ativa");
+    pergunta.set.getAttribute("aria-expanded", ativa);
+        
+} 
+function eventosPerguntas(pergunta) {
+   pergunta.addEventListener('click', ativarPergunta);
+}
+perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+console.log(galeria, galeriaContainer);
