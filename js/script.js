483a2e0b46a30ed256ad1267a3b692ff73aee91f
myllenabitar <myllena.odo@gmail.com>
2023-10-06 16:42:09 -0400

    js na galeria de fotos

--- js/script.js
+++ js/script.js
@@ -44,4 +44,16 @@ perguntas.forEach(eventosPerguntas);
 const galeria = document.querySelectorAll(".bicicleta-imagens img");
 const galeriaContainer = document.querySelector(".bicicleta-imagens");
 
-console.log(galeria, galeriaContainer);
+function trocarImagem(event) {
+    const img = event.currentTarget;
+    const media = matchMedia('(min-width:1000px;)').matches;
+    if(media) {
+    galeriaContainer.prepend(img);
+    }
+}
+
+function eventosGaleria(img) {
+    img.addEventListener('click', trocarImagem);
+    }
+
+galeria.forEach(eventosGaleria);
