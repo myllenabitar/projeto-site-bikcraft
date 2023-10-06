d6dbadfb74575a28f410b707028bbe0fa4267ffe
myllenabitar <myllena.odo@gmail.com>
2023-10-06 16:43:58 -0400

    js na galeria/seleção de fotos de bicicleta

--- js/script.js
+++ js/script.js
@@ -46,9 +46,9 @@ const galeriaContainer = document.querySelector(".bicicleta-imagens");
 
 function trocarImagem(event) {
     const img = event.currentTarget;
-    const media = matchMedia('(min-width:1000px;)').matches;
+    const media = matchMedia('(min-width:1000px)').matches;
     if(media) {
-    galeriaContainer.prepend(img);
+        galeriaContainer.prepend(img);
     }
 }
 
